670c24e22e1a424647d86d1ddbebaa49021d8481
James-Muthama <jamesmuthaiks@gmail.com>
2023-11-30 21:24:59 +0300

    modified prescription.js and admin_pgae.js

--- models/prescription.js
+++ models/prescription.js
@@ -3,32 +3,54 @@ const Schema = mongoose.Schema;
 const {isEmail} = require('validator');
 const bcrypt = require('bcrypt');
 
-const patientSchema = new Schema({
-  firstname: {
-    type: String,
-    required :[true, 'Please enter First Name']
-  },
-  lastname: {
-    type: String,
-    required : [true, 'Please enter Last Name']
-  },
-  gender: {
-    type: String,
-    required :[true, 'Please choose a gender']
-  },
-  DOB: {
-    type: String,
-    required :[true, 'Please enter Date of Birth']
-  },
-  email: {
+const prescriptionSchema = new Schema({
+  patient_email: {
     type: String,
     required :[true, 'Please enter valid Email'],
     unique:true,
     validate: [isEmail, 'Invalid email']
   },
-  password: {
+  docotor_email: {
     type: String,
-    required :[true, 'Please enter valid password'],
-    minlength: [8, 'Minimum length of password is 8']
+    required :[true, 'Please enter valid Email'],
+    unique:true,
+    validate: [isEmail, 'Invalid email']
+  },
+  symptoms: {
+    type: String,
+    required :[true, 'Please enter a symptoms']
+  },
+  drug_name: {
+    type: String,
+    required :[true, 'Please enter a drug name']
+  },
+  quantity: {
+    type: String,
+    required :[true, 'Please enter valid quantity'],
+  },
+  dosage: {
+    type: String,
+    required :[true, 'Please enter valid quantity'],
+  },
+  quantity: {
+    type: String,
+    required :[true, 'Please enter valid quantity'],
+  },
+  start_date: {
+    type: String,
+    required :[true, 'Please enter Prescription Start Date'],
+  },
+  end_date: {
+    type: String,
+    required :[true, 'Please enter Prescription End Date'],
+  },
+  price: {
+    type: String,
+    required :[true, 'Please enter price'],
   },
 }, {timestamps: true});
+
+
+const Prescription = mongoose.model('prescription', prescriptionSchema);
+
+module.exports = Prescription;
